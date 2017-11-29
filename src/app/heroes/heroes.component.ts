4015b88cfc1e5614c06ddb5158a31ab66ec65291
TedtheDev <tylerjedmunds@gmail.com>
2017-11-28 23:50:04 -0600

    added router, added router-outlet to AppComponent, added AppRoutingModule, added 4 routes, refactored HeroDetailComponent

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,8 +11,6 @@ import { HeroService } from '../hero.service';
 
 export class HeroesComponent implements OnInit {
 
-  selectedHero: Hero;
-
   heroes: Hero[];
 
   constructor(private heroService: HeroService, ) { }
@@ -24,10 +22,6 @@ export class HeroesComponent implements OnInit {
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
   }
 
 }
